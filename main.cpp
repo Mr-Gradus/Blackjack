e44b8e1bceb661e9dfe0e298374115e97c3e1b37
Mr-Gradus <rybin.dmitriy@gmail.com>
2021-12-08 21:04:10 +0500

    Start project
    Скелет программы примерный

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <locale>
+#include <string>
+
+using namespace std;
+
+class Card {
+private:
+	string suit;
+	char value;
+    
+public:
+
+
+};
+
+class Hand {
+
+};
+
+class Deck : private Hand {
+
+};
+
+class GenericPlayer : private Hand {
+
+};
+
+class Player : private GenericPlayer {
+
+};
+
+class House : private GenericPlayer {
+
+};
+
+class Game {
+
+};
+
+int main()
+{
+
+
+
+
+	return 0;
+}
